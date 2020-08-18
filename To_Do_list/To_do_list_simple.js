38e69be081ce5e31be49262a0613af532b5d272c
Akshay-ch-dj <akshaych203@gmail.com>
2020-08-18 16:20:45 +0530

    Added The local storage feature.
    using some primitive tech, but it can be improved

--- To_Do_list/To_do_list_simple.js
+++ To_Do_list/To_do_list_simple.js
@@ -1,22 +1,30 @@
-const newTask = document.querySelector('#new-task-input'); 
-/* To grab the user input[can be used to change the placeholder in the box, 
-    later 2nd phase] */
+// #################  GET ITEMS/QUERY SELECTORS ###########
 
+// To grab the user input 
+const newTask = document.querySelector('#new-task-input'); 
 // To interact with the "+" button
 const addTaskBtn = document.querySelector('.add-task-btn');
 // To interact with the "clear complete" button
 const clearCompleteBtn = document.querySelector('.clear-completed-btn');
 // grab the task list ul element(it is needed to add the template)
 const taskList = document.querySelector('.task-list');
-// finally, grabbing the template add content
+// finally, getting the template add content
 const taskTemplate = document.querySelector('#task-template');
 
+// ################# Global Variables & const. #############
 // Need an id assigned for each task element created, start with id=1
 let id = 1;
+// global var. for local storage array
+let localList;
 
-// Take newTask(the input) add an event-listener for a keypress(enter) and "+"
+// ############ Event Listeners #############
 
-/* If pressed key is enter(keycode = 13) and the input is valid(validation for
+// Event listener for page loading
+document.addEventListener('DOMContentLoaded', getFromLocal);
+
+// Event-listener for a keypress(enter) and "+"
+
+/* If pressed key is enter and the input is valid(validation for
     only blank line, fn. below), then add task (fn. below)*/
 newTask.addEventListener('keypress', (e) => {
    if (e.key === 'Enter' && inputValid()){
@@ -32,7 +40,6 @@ addTaskBtn.addEventListener('click', () => {
 });
 
 // To clear the completed taskList items
-
 clearCompleteBtn.addEventListener('click', () => {
     /* All list template elements with class task, create an array and loop 
     through each, check the input-checked/not, remove the checked ones */
@@ -40,13 +47,15 @@ clearCompleteBtn.addEventListener('click', () => {
     tasks.forEach(task => {
         const checked = task.querySelector('input').checked;
         if (checked) {
+            removeFromLocal(task);
             task.remove();
         }
     })
 });
 
-// The addTask function- The main fun.
+// ################# Functions ###################
 
+// The addTask function- The main fun.
 function addTask() {
     /* the taskElement const. set up with a new task element(template), 
     If set false only top level element(here <li></li>)- gets pulled, 
@@ -63,7 +72,11 @@ function addTask() {
     label.append(newTask.value);
     // add the modified template content(taskElement) to the ul(taskList)
     // appendChild adds a new node as child element to given node
-    taskList.appendChild(taskElement);
+    taskList.appendChild(taskElement); 
+
+    // local saving
+    saveToLocal(newTask.value);
+
     // empty the input box(newTask) to take in a new task
     newTask.value = '';
     // increment the id++;
@@ -75,4 +88,52 @@ function addTask() {
 function inputValid(){
     // if input not equal to "empty-string" return true
     return newTask.value !== ''
+}
+
+
+//$$$$$$$$$$$$$ Adding the local storage feature  $$$$$$$$$$$
+ 
+// Check for local storage(helper fun.)
+function checkForLocal() {
+    if (localStorage.getItem('localList') === null){
+        // If no item named 'localList' setup an empty array
+        localList = [];
+    } else {
+        localList = JSON.parse(localStorage.getItem('localList'));
+    }
+}
+
+// SAVE to local storage
+function saveToLocal(InputValue) {
+    // check
+    checkForLocal();
+    localList.push(InputValue);
+    localStorage.setItem('localList', JSON.stringify(localList));
+}
+
+// RETRIEVE from local storage
+function getFromLocal() {
+    checkForLocal();
+    localList.forEach(listItem => {
+        // Need to replicate the add functionality
+        const taskElement = document.importNode(taskTemplate.content, true); 
+        const checkbox = taskElement.querySelector('input');
+        checkbox.id = id;
+        const label = taskElement.querySelector('label');
+        label.htmlFor = id;
+        
+        label.append(listItem);
+        
+        taskList.appendChild(taskElement);
+        id++;
+    });
+}
+// DELETE from local storage
+function removeFromLocal(task) {
+    /* task is the single list item, grab only the text from it, then reverse
+       search for to get the index and remove the element using that index.*/
+    const taskIndex = localList.indexOf(task.children[1].innerText.slice(3));
+    localList.splice(taskIndex, 1);
+    // Set the modified array to local storage
+    localStorage.setItem('localList', JSON.stringify(localList));
 }
