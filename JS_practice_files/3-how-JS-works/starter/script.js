08a5ee5c5bd69bbeea653bb786f0a5321119bf33
Akshay-ch-dj <akshaych203@gmail.com>
2020-08-24 21:47:22 +0530

    Completed the js working part.
    The hoisting, execution contesxt and this keyword

--- JS_practice_files/3-how-JS-works/starter/script.js
+++ JS_practice_files/3-how-JS-works/starter/script.js
@@ -1,20 +1,103 @@
 ///////////////////////////////////////
-// Lecture: Hoisting
+// // Lecture: Hoisting
 
+// calculateAge(1995);
 
+// function calculateAge(year) {
+//     console.log(2020 - year);
+// }
 
+// // yearRet(1995);
 
+// var yearRet = function(year) {
+//     console.log(65 - (2020 - year));
+// }
 
+// // In variables
+// console.log(age);  // Trying to use the variable before declaring
+// var age = 23;
+// console.log(age);
 
+// // Scope of variable
 
+// function foo() {
+//     console.log(age);
+//     var age = 65;
+//     console.log(age);
+// }
 
+// foo();
+// console.log(age);
 
+// Scope of a variable
 
+// var x = 'Hello';
 
+// first();
 
+// // lexical scoping
+// function first() {
+//     var y = ' local';
+//     second();
+//     function second() {
+//         var z = ' and nested';
+//         console.log(x + y + z); 
+//     }
+// }
 
+// var a = 'Hello!';
+// first();
 
+// function first() {
+//     var b = 'Hi!';
+//     second();
+//     function second() {
+//         var c ='Hey!';
+//         third();
+//     }
+// }
 
+// function third() {
+//     var d = 'John';
+//     console.log(a + b + c + d);
+// }
+
+// // The 'this' keyword
+
+// console.log(this)
+// calcAge(1990);
+// function calcAge(year) {
+//     console.log(2020 - year);
+//     console.log(this);
+// }
+
+// // this in an object
+// var john = {
+//     name: 'John',
+//     yearOfBirth: 1990,
+//     calculateAge: function() {
+//         console.log(this);
+//         console.log(2020 - this.yearOfBirth);
+
+//         /*
+//         function innerFunction() {
+//             console.log(this);
+//         }
+//         innerFunction();
+//         */
+//     }
+// }
+
+// john.calculateAge();
+
+// var mike = {
+//     name: 'Mike Tyson',
+//     yearOfBirth: 1994,
+
+// }
+
+// mike.calculateAge = john.calculateAge;
+// mike.calculateAge();
 
 
 ///////////////////////////////////////
