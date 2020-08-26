f0f1c47f253bf671ab7c1b48a9c873b0c3e2b483
Akshay-ch-dj <akshaych203@gmail.com>
2020-08-26 17:22:55 +0530

    Pig game version 2 challenge completed.

--- JS_practice_files/4-DOM-pig-game/starter/app.js
+++ JS_practice_files/4-DOM-pig-game/starter/app.js
@@ -11,7 +11,7 @@ GAME RULES:
 
 // ************** Declaring Global variables ************** //
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, prvDice1, prvDice2, targetVal;
 
 /************* Functions **********************/
 
@@ -28,7 +28,8 @@ function resetGame() {
     activePlayer = 0;
 
     // Hide the dice by setting its display property to none.
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 
     [0, 1].forEach((num) => {
         // Set the initial values of players scores to zero
@@ -43,7 +44,14 @@ function resetGame() {
     });
 
     document.querySelector(`.player-0-panel`).classList.add('active');
-    document.querySelector('.dice').classList.add('animate1');
+
+    document.querySelector('.dice1').classList.add('animate1');
+    document.querySelector('.dice2').classList.add('animate1');
+    
+    // Set the target
+    var tempVal = document.getElementById('target').value;
+    targetVal = tempVal >= 10 ? tempVal : 100;
+    document.querySelector('.target-show').textContent = `Target Score: ${targetVal}`;
 }
 
 function playerChange() {
@@ -58,7 +66,7 @@ function playerChange() {
     document.querySelector(`.player-0-panel`).classList.toggle('active');
     document.querySelector(`.player-1-panel`).classList.toggle('active');
 
-    // document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice').style.display = 'none';
 }
 
 
@@ -72,26 +80,45 @@ resetGame();
 // Adding the roll button click-event listener,
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
-    document.querySelector('.dice').classList.toggle('animate1');
-    document.querySelector('.dice').classList.toggle('animate2');
-    
+    // For the animation class
+    document.querySelector('.dice1').classList.toggle('animate1');
+    document.querySelector('.dice1').classList.toggle('animate2');
+    document.querySelector('.dice2').classList.toggle('animate1');
+    document.querySelector('.dice2').classList.toggle('animate2');
     
     if(gamePlaying) {
+
         // 1. When ROLL DICE is clicked, random number(b/w 1-6) generated.
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
         // 2. Get the dice image corresponding to that number
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
+        var diceDOM1 = document.querySelector('.dice1');
+        var diceDOM2 = document.querySelector('.dice2');
+
+        diceDOM1.style.display = 'block';
+        diceDOM2.style.display = 'block';
         // The dice images are named accordingly, change the src attribute to change the img
-        diceDOM.src = `https://github.com/Akshay-ch-dj/Javascript-experience-projects/blob/master/JS_practice_files/4-DOM-pig-game/starter/dice-${dice}.png?raw=true`;
-        
+        diceDOM1.src = `https://github.com/Akshay-ch-dj/Javascript-experience-projects/blob/master/JS_practice_files/4-DOM-pig-game/starter/dice-${dice1}.png?raw=true`;
+        diceDOM2.src = `https://github.com/Akshay-ch-dj/Javascript-experience-projects/blob/master/JS_practice_files/4-DOM-pig-game/starter/dice-${dice2}.png?raw=true`;
 
         // 3. Update the roundScore if the number is not 1
-        if (dice !== 1) {
-            // Add score
-            roundScore += dice;
-            document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+        if (dice1 !== 1 && dice2 !== 1) {
+            // Check for repeated 6
+            if (dice1 + prvDice1 === 12 || dice2 + prvDice2 === 12) {
+                playerChange();
+                prvDice1 = 0;
+                prvDice2 = 0;
+            } else {
+                // Add score
+                var dice = dice1 + dice2;
+                roundScore += dice;
+                document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+                // Store the dice value
+                prvDice1 = dice1;
+                prvDice2 = dice2;
+            }
+
         } else {
             playerChange();
         }
@@ -106,10 +133,12 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // Update the UI
         document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+
+        if (scores[activePlayer] >= targetVal) {
             // Add winner class to the player active panel
             document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice1').style.display = 'none';
+            document.querySelector('.dice2').style.display = 'none';
             document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
             document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
             gamePlaying = false;
@@ -125,14 +154,13 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 document.querySelector('.btn-new').addEventListener('click', resetGame);
 
 
+/*
+3 Challenges
 
-
-// ***************** DOM Manipulation ****************** /
-// Use querySelector to select the classes and styles as in CSS, 
-// Withe help of activePlayer variable: "setter"
-// document.querySelector(`#current-${activePlayer}`).textContent = dice; 
-// document.querySelector(`#current-${activePlayer}`).innerHTML = 
-// `<em>${dice}<em>`
-
-// // To read the value of the content: "getter"
-// var x = document.querySelector('#score-0').textContent;
+1. A player looses his entire Score when he rolls two 6 in a row, after that, its the next
+players turn. (separate variable to save prv. dice roll)
+2. Add an input field to the HTML where players can set the winning score, to change the predefined 
+score of 100. (Hint: use the 'value' property in js)
+3. Add another dice to the game, there are two dices now. The player looses his current score when 
+one of them is a 1. 
+*/
