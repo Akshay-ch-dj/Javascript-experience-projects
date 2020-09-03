8295f1068db821769244a10101808cebf5f6d782
Akshay-ch-dj <akshaych203@gmail.com>
2020-09-03 14:44:56 +0530

    Init function created in the budget app,
    To call all event listeners and the budget
    controller initialized withobjects and arrays to store
    fetched data.

--- JS_practice_files/6-budgety/starter/app.js
+++ JS_practice_files/6-budgety/starter/app.js
@@ -2,6 +2,32 @@
 // BUDGET CONTROLLER:- For controlling Data
 var budgetController = (function () {
 
+    // Data storing medium, object with descriptions, values and id
+    // Constructor
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    // For income
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    // All income and expense objects created stored in separate arrays, in a data object
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
 })();
 
 
@@ -9,6 +35,7 @@ var budgetController = (function () {
 // UI CONTROLLER:- For controlling UI elements
 var UIController = (function() {
 
+    // classes assigned
     var DOMStrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
@@ -16,12 +43,13 @@ var UIController = (function() {
         btnOK: '.add__btn'
     };
 
-    // (2). Public function:- To read data from the input fields
+    // (2). Public functions
     return {
         getInput: function() {
             return {
                 // 3 i/p to read: i) income/expense, ii) description iii) amount
                 // resp. classes: 'add__type', 'add__description', 'add__value'
+                // Assigned to DOMStrings
                 type: document.querySelector(DOMStrings.inputType).value, // values
                 // 'inc' and 'exp'.
                 description: document.querySelector(DOMStrings.inputDescription).value,
@@ -37,16 +65,29 @@ var UIController = (function() {
 
 
 
-// GLOBAL APP CONTROLLER:- For Interconnecting the independent modules,
-// pass them as arguments
+// GLOBAL APP CONTROLLER:- For Interconnecting the independent modules, passed as arguments
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMStrings();
+    var setupEventListeners = function() {
+        // Fetched the DOM strings
+        var DOM = UICtrl.getDOMStrings();
+
+        // (1). The listener for the add button(tick_mark):- that adds new data
+        document.querySelector(DOM.btnOK).addEventListener('click', ctrlAddItem);
+
+        // (1).a For the key_press on the add button (Enter button), use the
+        // Keypress event, listens when any key is pressed(except:- shift & some fn. keys)
+        document.addEventListener('keypress', function (event) {
+            // keycode:- console.log(event)-for enter:- '13', 'event.which' for the older browsers.
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        })
+    }
 
     var ctrlAddItem = function() {
-        // 1. Get the filled input data
+        // 1. Get the filled input data as an object
         var input = UICtrl.getInput();
-        console.log(input);
         // 2. Add the item to the budget control for calcs.
 
         // 3. Modify the UI
@@ -56,18 +97,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 5. Display the budget on top UI.
     }
 
-    // (1). The listener for the add button(tick_mark):- that adds new data
-    document.querySelector(DOM.btnOK).addEventListener('click', ctrlAddItem);
-
-    // (1).a For the key_press on the add button (Enter button), use the
-    // Keypress event, listens when any key is pressed(except:- shift & some fn. keys)
-    document.addEventListener('keypress', function(event) {
-        // identify the keycode using:- console.log(event)-for enter:- '13'
-        // 'event.which' for the older browsers.
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    // create a public 'init'(initialization) function to call event-listeners
+    return {
+        init: function() {
+            setupEventListeners();
         }
-    })
+    };
 
 })(budgetController, UIController);
 // The above are the real parameters passed
+
+// Initializes all event listeners
+controller.init();
