c024b2d23d85f669a8d7020b9f77d8411cfb5cbd
Akshay-ch-dj <akshaych203@gmail.com>
2020-12-04 21:25:27 +0530

    Js closure challenge completed

--- JS_practice_files/4-ES6/IIFE_Closures.js
+++ JS_practice_files/4-ES6/IIFE_Closures.js
@@ -90,7 +90,7 @@ const boardPassengers = function (n, wait) {
 };
 
 boardPassengers(180, 3);
-
+*/
 /*
 Will start boarding in 3 seconds
 After 3 seconds
@@ -100,5 +100,33 @@ There are 3 groups each with 60 passengers
 
 // The callback function can able to use all variables in the parent exec context
 // while it is closed at immediately,
+
+/*
+function sampleFun(cars) {
+  console.log(cars.trey?.color ?? 'No trey');
+}
+
+sampleFun({});
+// logs "No trey"
+*/
+
+// Closure challenge
+// Coding Challenge #2
+
+/*
+This is more of a thinking challenge than a coding challenge 🤓
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
 
 */
