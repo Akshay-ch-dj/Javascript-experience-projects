ebd5df87346ac1c7e03c00e44221a796a106f2da
Akshay-ch-dj <akshaych203@gmail.com>
2020-12-07 23:59:37 +0530

    Added images and configured the given css

--- Bankist_app/script.js
+++ Bankist_app/script.js
@@ -59,10 +59,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {};
+
 /// //////////////////////////////////////////////
 /// //////////////////////////////////////////////
 // LECTURES
-
+/*
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -71,3 +73,4 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+*/
